6f4b2f03bdab3734466eb69d28c813dbada1d9f8
Shinder Lin <shinder.lin@gmail.com>
2025-12-10 08:57:17 +0800

    title 修改測試

--- src/views/components/Navbar.vue
+++ src/views/components/Navbar.vue
@@ -6,7 +6,7 @@ const cart = useCartStore()
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <RouterLink class="navbar-brand" to="/">Navbar</RouterLink>
+      <RouterLink class="navbar-brand" to="/">新德</RouterLink>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
